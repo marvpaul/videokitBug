d26037a3385bb6ddcd99fa031edaebc147c5bf8a
marvpaul <marvin.krueger@yahoo.de>
2024-04-29 23:08:56 +0200

    New bug: Audio recording dosen't work

--- Assets/Recorder.cs
+++ Assets/Recorder.cs
@@ -5,6 +5,17 @@ using VideoKit;
 public class Recorder : MonoBehaviour
 {
     public void StartRecording(){
+        if (GetComponent<VideoKitRecorder>().status == VideoKitRecorder.Status.Recording){
+
+        this.GetComponent<VideoKitRecorder>().StopRecording(); 
+        } else {
         this.GetComponent<VideoKitRecorder>().StartRecording(); 
+        }
+    }
+
+    public async void StopRecording(MediaAsset asset)
+    {
+        var recordingPath = asset.path;
+        Debug.Log(recordingPath);
     }
 }
